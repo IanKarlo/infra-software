d90c7054951a349bed947b6de76611063c537d68
Alef-0 <AlefGabryel@protonmail.com>
2020-10-19 09:19:51 -0300

    Mudanças
    O free na função as vezes nao dava tempo de imprimir qual thread terminava.
    Formatação.

--- Threads/q1/main.c
+++ Threads/q1/main.c
@@ -10,53 +10,36 @@ pthread_t *threads;               // Vetor de threads
 /* Funções do Programa */
 
 void *ThreadFunc(void *arg) {    //Função da thread 
-
     while(i < 1000000){
-
       pthread_mutex_lock(&lock);
-      if(i == 1000000){
-        printf("A thread com ID: %lu, alcançou o limite!\n",pthread_self());
-        i++;
-        free(threads);
-        exit(0);
-      }
       i++;
+      if(i == 1000000){printf("A thread com ID: %lu, alcançou o limite!\n",pthread_self());}
       pthread_mutex_unlock(&lock);
     }
-
-    return NULL;
+    pthread_exit(NULL);
 } 
 
 /* Main */
    
 int main() {
-
     int n;
-
     if (pthread_mutex_init(&lock, NULL) != 0) {
       puts("\n Erro na inicialização do mutex!\n");
       return 1;
     }
-
     printf("Digite a quantidade de threads: ");
     scanf("%d", &n);
-
-    if(n < 0) {
+    if(n < 1) {
       puts("Valor invalido!");
       exit(-1);
     }
-
     threads = (pthread_t*) malloc(n * sizeof(pthread_t));
     if(threads == NULL){
       puts("Erro de alocação!\nPrograma encerrado!");
       exit(-1);
     }
-
-    for(int j = 0; j < n; j++) {
-      pthread_create(&threads[j], NULL, ThreadFunc, NULL); 
-    }
-    for(int j = 0; j < n; j++) {
-      pthread_join(threads[j], NULL); 
-    }
-    exit(0); 
+    for(int j = 0; j < n; j++) {pthread_create(&threads[j], NULL, ThreadFunc, NULL);}
+    for(int j = 0; j < n; j++) {pthread_join(threads[j], NULL);}
+    free(threads);
+    pthread_exit(NULL);
 }

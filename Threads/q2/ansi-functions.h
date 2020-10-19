2bdb7e80ffde76647e4c3d44b39f5cf1d3a41eb9
Alef-0 <AlefGabryel@protonmail.com>
2020-10-19 17:29:30 -0300

    Uptade da tarde da questão 2
    Tudo está agora em uma thread.

--- Threads/q2/ansi-functions.h
+++ Threads/q2/ansi-functions.h
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+// pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
 // Funções para mover o cursor
 void left(int n)    {printf("\u001b[%dD", n);}
 void right(int n)   {printf("\u001b[%dC", n);}
@@ -26,3 +30,45 @@ void print_black()  {printf("\033[38;2;255;255;255m"); printf("\033[48;2;0;0;0m"
 void print_cyan()   {printf("\033[38;2;0;0;0m"); printf("\033[48;2;0;255;255m");}
 
 void reset(){printf("\e[0m");}
+
+// imprimir tudo certinho
+typedef struct  imprimir{
+    char codigo[10];
+    char cidade[15];
+    char horario[10];
+    int linha;
+}imprimir;
+
+void print_line(char* codigo, char* cidade, char* horario){
+    printf("%s %s",codigo,cidade);
+    int espacos = 13 - strlen(cidade);
+    for (int i=0;i<espacos;i++)printf(" ");
+    printf("%s",horario);
+    fflush(stdout);
+}
+
+void func_main(int total, imprimir str){
+    // pthread_mutex_lock(mutex);
+    ret_pos();
+    next_line(str.linha);
+    switch(str.linha % 7){
+        case 0: {print_red(); break;}
+        case 1: {print_yellow(); break;}
+        case 2: {print_blue(); break;}
+        case 3: {print_magenta(); break;}
+        case 4: {print_green(); break;}
+        case 5: {print_black(); break;}
+        case 6: {print_cyan(); break;}
+    }
+    print_line(str.codigo,str.cidade,str.horario);
+    reset();
+    next_line(total-str.linha+1); printf(" "); //Apenas para ir para lá
+    sleep(2);
+    // pthread_mutex_unlock(mutex);
+}
+
+void start(){
+    up(11); //Só para ficar no topo de algo
+    save_pos();
+    erase_display(); //Isso só funciona pra ficar tudo em uma mesma linha
+}
--- Threads/q2/main.c
+++ Threads/q2/main.c
@@ -1,23 +1,75 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
 #include "ansi-functions.h"
+
+pthread_t *threads;
+pthread_mutex_t *mutexes;
+imprimir* str;
+int total;
+
+void *funcao(void* args);
 
 int main(void)
 {
-    print_red();
-    printf("                         \n");
-    print_yellow();
-    printf("AQT123 Frankfurt    20:00\n");
-    print_blue();
-    printf("AQT123 Frankfurt    20:00\n");
-    print_magenta();
-    printf("AQT123 Frankfurt    20:00\n");
-    print_green();
-    printf("AQT123 Frankfurt    20:00\n");
-    print_black();
-    printf("AQT123 Frankfurt    20:00\n");
-    print_cyan();
-    printf("AQT123 Frankfurt    20:00\n");
-    reset();
+    start(); //Funcao para colocar a quantidade de linha
+    int quant_arquivos;
+    printf("Quantos arquivos, serao? ");
+    scanf(" %d", &quant_arquivos);
+    
+    FILE **file = (FILE**) malloc(sizeof(file)*quant_arquivos);
+    for(int i=0;i<quant_arquivos;i++){
+        printf("Digite o nome do arquivo (%d): ", i);
+        char frase[30];
+        scanf(" %s",frase);
+        file[i] = fopen(frase,"r");
+    }
+
+    int linha;
+    printf("Quantas linhas no total?: ");
+    scanf(" %d", &total);
+    threads = (pthread_t *) malloc( sizeof(pthread_t)*total);
+    mutexes = (pthread_mutex_t *) malloc( sizeof(pthread_mutex_t) * total);
+    for(int i=0;i<total;i++) pthread_mutex_init(&mutexes[i],NULL);
+    str = (imprimir*) malloc(sizeof(imprimir) * total);
+
+
+    //Une tudo em um arquivo só
+    char tudo[40];
+    FILE* novo = fopen("tudo.txt","w");
+    for(int i=0;i<quant_arquivos;i++){
+        while(fscanf(file[i]," %[^\n]\n", tudo) != EOF){
+            fprintf(novo,"%s\n",tudo);
+        }
+    }
+    fclose(novo);
+
+    //Apagar tudo antes de começar
+    ret_pos();
+    erase_display();
+
+    //Começa agora os casos
+    novo = fopen("tudo.txt","r");
+    while(fscanf(novo," %d", &linha) != EOF){
+        fscanf(novo," %s %s %s", str[linha-1].codigo, str[linha-1].cidade, str[linha-1].horario);
+        str[linha-1].linha = linha;
+        pthread_create(&threads[linha-1],NULL,funcao,&linha);
+        pthread_join(threads[linha-1],NULL);
+    }
+    
+    for (int i = 0; i<total;i++) pthread_join(threads[i],NULL);
+    //Libera todas as memorias alocadas
+    free(file);
+    free(mutexes);
+    free(threads);
+    free(str);
+    remove("tudo.txt");
+}
+
+void *funcao(void* args){
+    int *linha = (int*) args;
+    func_main(total,str[(*linha)-1]);
+    pthread_exit(NULL);
 }
